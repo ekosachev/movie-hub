7cb7d6486a01ef1f110c151113799e739b38ed7f
Egor Kosachev <egorkosachev@vk.com>
2026-04-07 19:19:30 +0300

    fix: make arguments in BaseRepo of generic type T

--- back/database/base_repository.go
+++ back/database/base_repository.go
@@ -4,9 +4,9 @@ import (
 	"context"
 )
 
-type BaseRepository interface {
-	Create(ctx context.Context, obj *BaseRepository) error
-	Query(ctx context.Context, filter *BaseRepository) ([]BaseRepository, error)
-	Update(ctx context.Context, filter *BaseRepository, obj *BaseRepository) (rowsAffected int, err error)
-	Delete(ctx context.Context, filter *BaseRepository) error
+type BaseRepository[T any] interface {
+	Create(ctx context.Context, obj *T) error
+	Query(ctx context.Context, filter *T) ([]T, error)
+	Update(ctx context.Context, filter *T, obj *T) (rowsAffected int, err error)
+	Delete(ctx context.Context, filter *T) error
 }
