ed9562744c410b73205b3eefcd1b08120b5946fb
Egor Kosachev <egorkosachev@vk.com>
2026-04-07 19:39:34 +0300

    feat: add user repository

--- back/database/base_repository.go
+++ back/database/base_repository.go
@@ -7,6 +7,6 @@ import (
 type BaseRepository[T any] interface {
 	Create(ctx context.Context, obj *T) error
 	Query(ctx context.Context, filter *T) ([]T, error)
-	Update(ctx context.Context, filter *T, obj *T) (rowsAffected int, err error)
-	Delete(ctx context.Context, filter *T) error
+	Update(ctx context.Context, filter *T, obj T) (rowsAffected int, err error)
+	Delete(ctx context.Context, filter *T) (rowsAffected int, err error)
 }
