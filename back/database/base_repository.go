26e71b167c27220d0fefd30384449be9c33a42db
Egor Kosachev <egorkosachev@vk.com>
2026-04-07 19:17:07 +0300

    fix: make methods if BaseRepo public

--- back/database/base_repository.go
+++ back/database/base_repository.go
@@ -5,8 +5,8 @@ import (
 )
 
 type BaseRepository interface {
-	create(ctx context.Context, obj *BaseRepository) error
-	query(ctx context.Context, filter *BaseRepository) ([]BaseRepository, error)
-	update(ctx context.Context, filter *BaseRepository, obj *BaseRepository) (rowsAffected int, err error)
-	delete(ctx context.Context, filter *BaseRepository) error
+	Create(ctx context.Context, obj *BaseRepository) error
+	Query(ctx context.Context, filter *BaseRepository) ([]BaseRepository, error)
+	Update(ctx context.Context, filter *BaseRepository, obj *BaseRepository) (rowsAffected int, err error)
+	Delete(ctx context.Context, filter *BaseRepository) error
 }
