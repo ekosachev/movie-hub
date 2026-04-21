34346bdf5ee2207ee2dd28219a0155abcbdac1c6
Egor Kosachev <egorkosachev@vk.com>
2026-04-21 13:52:57 +0300

    remove user handler to rewrite

--- back/internal/dto/user.go
+++ back/internal/dto/user.go
@@ -1,7 +1,1 @@
 package dto
-
-type CreateUserRequest struct {
-	Username     string `json:"username" binding:"required,alphaunicode"`
-	EmailAddress string `json:"email_address" binding:"required,email"`
-	Password     string `json:"password" binding:"required,alphanumunicode"`
-}
--- back/internal/handlers/user_handler.go
+++ back/internal/handlers/user_handler.go
@@ -1,61 +1,11 @@
 package handlers
 
 import (
-	"log/slog"
-	"net/http"
-
-	"github.com/ekosachev/movie-hub/internal/dto"
-	"github.com/ekosachev/movie-hub/internal/models"
 	"github.com/ekosachev/movie-hub/internal/services"
-	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
+	"log/slog"
 )
 
 type UserHandler struct {
 	service *services.UserService
 	logger  *slog.Logger
-}
-
-func NewUserHandler(service *services.UserService, logger *slog.Logger) *UserHandler {
-	return &UserHandler{
-		service: service,
-		logger:  logger,
-	}
-}
-
-func (h *UserHandler) RegisterRoutes(router *gin.RouterGroup, path string) {
-	group := router.Group(path)
-
-	{
-		group.POST("/create", h.CreateUser)
-	}
-}
-
-func (h *UserHandler) CreateUser(c *gin.Context) {
-	var req dto.CreateUserRequest
-
-	if err := c.ShouldBindJSON(&req); err != nil {
-		c.JSON(http.StatusBadRequest, 
-			dto.APIResponse {
-				Success: false,
-				Error: err.Error(),
-			}
-	)
-		return
-	}
-
-	user := &models.User{
-		Model:        gorm.Model{},
-		EmailAddress: req.EmailAddress,
-		Username:     req.Username,
-		PasswordHash: req.Password,
-	}
-
-	if err := h.service.Create(c, user); err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal Server Error"})
-		h.logger.Error("Error when creating a user", "error", err.Error())
-		return
-	}
-
-	c.JSON(http.StatusCreated, gin.H{"message": "User Created"})
 }
