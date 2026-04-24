cdc1680a4d6952000264783deb91d1decb2b48f9
Amkaus <amkkaus@gmail.com>
2026-04-24 18:49:55 +0300

    added comment handler

---
+++ back/internal/dto/comment.go
@@ -0,0 +1,20 @@
+package dto
+
+type CreateCommentRequest struct {
+	Content         string `json:"content" binding:"required"`
+	ParentCommentID *int   `json:"parent_comment_id,omitempty"`
+	UserID          int    `json:"user_id" binding:"required"`
+	MovieID         int    `json:"movie_id" binding:"required"`
+}
+
+type UpdateCommentRequest struct {
+	Content *string `json:"content" binding:"required"`
+}
+
+type CommentResponse struct {
+	ID              uint   `json:"id"`
+	Content         string `json:"content"`
+	ParentCommentID *int   `json:"parent_comment_id,omitempty"`
+	UserID          int    `json:"user_id"`
+	MovieID         int    `json:"movie_id"`
+}
