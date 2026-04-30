0cb063e94736c96702cab35278c620685fa08b71
Egor Kosachev <egorkosachev@vk.com>
2026-04-30 16:35:48 +0300

    feat: add movie filter dto

--- back/internal/dto/movie.go
+++ back/internal/dto/movie.go
@@ -1,5 +1,7 @@
 package dto
 
+import "time"
+
 type CreateMovieRequest struct {
 	Title       string `json:"title" binding:"required"`
 	Description string `json:"description" binding:"required"`
@@ -17,4 +19,12 @@ type UpdateMovieRequest struct {
 	Title       *string `json:"title" binding:"omitempty"`
 	Description *string `json:"description" binding:"omitempty"`
 	ReleaseDate *string `json:"release_date" binding:"omitempty,datetime=DateTime"`
+}
+
+type MovieFilterRequest struct {
+	Title     string     `form:"title"`
+	TagIDs    []uint     `form:"tag_ids"`
+	DateFrom  *time.Time `form:"date_from" time_format:"2006-01-02"`
+	DateTo    *time.Time `form:"time_to" time_format:"2006-01-02"`
+	MinRating float64    `form:"min_rating"`
 }
