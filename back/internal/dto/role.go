d8525ef12b4dc24079e527c98fac6cb8d378e156
Egor Kosachev <egorkosachev@vk.com>
2026-05-01 13:32:21 +0300

    feat: update role to include role name

--- back/internal/dto/role.go
+++ back/internal/dto/role.go
@@ -1,23 +1,26 @@
 package dto
 
 type CreateRoleRequest struct {
-	CanDeleteUsers  bool `json:"can_delete_users" binding:"required"`
-	CanUpdateMovies bool `json:"can_update_movies" binding:"required"`
-	CanUpdateRoles  bool `json:"can_update_roles" binding:"required"`
-	CanUpdateTags   bool `json:"can_update_tags" binding:"required"`
+	Name            string `json:"name" binding:"required,min=3"`
+	CanDeleteUsers  bool   `json:"can_delete_users" binding:"required"`
+	CanUpdateMovies bool   `json:"can_update_movies" binding:"required"`
+	CanUpdateRoles  bool   `json:"can_update_roles" binding:"required"`
+	CanUpdateTags   bool   `json:"can_update_tags" binding:"required"`
 }
 
 type RoleResponse struct {
-	ID              uint `json:"id"`
-	CanDeleteUsers  bool `json:"can_delete_users"`
-	CanUpdateMovies bool `json:"can_update_movies"`
-	CanUpdateRoles  bool `json:"can_update_roles"`
-	CanUpdateTags   bool `json:"can_update_tags"`
+	ID              uint   `json:"id"`
+	Name            string `json:"name"`
+	CanDeleteUsers  bool   `json:"can_delete_users"`
+	CanUpdateMovies bool   `json:"can_update_movies"`
+	CanUpdateRoles  bool   `json:"can_update_roles"`
+	CanUpdateTags   bool   `json:"can_update_tags"`
 }
 
 type UpdateRoleRequest struct {
-	CanDeleteUsers  *bool `json:"can_delete_users" binding:"omitempty"`
-	CanUpdateMovies *bool `json:"can_update_movies" binding:"omitempty"`
-	CanUpdateRoles  *bool `json:"can_update_roles" binding:"omitempty"`
-	CanUpdateTags   *bool `json:"can_update_tags" binding:"omitempty"`
+	Name            *string `json:"name" binding:"min=3"`
+	CanDeleteUsers  *bool   `json:"can_delete_users" binding:"omitempty"`
+	CanUpdateMovies *bool   `json:"can_update_movies" binding:"omitempty"`
+	CanUpdateRoles  *bool   `json:"can_update_roles" binding:"omitempty"`
+	CanUpdateTags   *bool   `json:"can_update_tags" binding:"omitempty"`
 }
