02879470d9bac4a5670dce9ff1dfd9ed43f108a6
Egor Kosachev <egorkosachev@vk.com>
2026-05-09 20:34:20 +0300

    did the thing

--- back/internal/repositories/role_repository.go
+++ back/internal/repositories/role_repository.go
@@ -15,15 +15,15 @@ func NewRoleRepository(db *gorm.DB) *RoleRepository {
 	return &RoleRepository{db: db}
 }
 
-func (r *RoleRepository) Create(ctx context.Context, obj *models.Role) error {
+func (r RoleRepository) Create(ctx context.Context, obj *models.Role) error {
 	return gorm.G[models.Role](r.db).Create(ctx, obj)
 }
 
-func (r *RoleRepository) Query(ctx context.Context, filter *models.Role) ([]models.Role, error) {
+func (r RoleRepository) Query(ctx context.Context, filter *models.Role) ([]models.Role, error) {
 	return gorm.G[models.Role](r.db).Where(filter).Find(ctx)
 }
 
-func (r *RoleRepository) GetByID(ctx context.Context, id uint) (*models.Role, error) {
+func (r RoleRepository) GetByID(ctx context.Context, id uint) (*models.Role, error) {
 	roles, err := r.Query(ctx, &models.Role{Model: gorm.Model{ID: id}})
 
 	if err != nil {
@@ -37,10 +37,14 @@ func (r *RoleRepository) GetByID(ctx context.Context, id uint) (*models.Role, er
 	return &roles[0], nil
 }
 
-func (r *RoleRepository) Update(ctx context.Context, filter *models.Role, obj models.Role) (int, error) {
+func (r RoleRepository) Update(ctx context.Context, filter *models.Role, obj models.Role) (int, error) {
 	return gorm.G[models.Role](r.db).Where(filter).Updates(ctx, obj)
 }
 
-func (r *RoleRepository) Delete(ctx context.Context, filter *models.Role) (int, error) {
+func (r RoleRepository) Delete(ctx context.Context, filter *models.Role) (int, error) {
 	return gorm.G[models.Role](r.db).Where(filter).Delete(ctx)
+}
+
+func (r RoleRepository) GetAll(ctx context.Context) ([]models.Role, error) {
+	return gorm.G[models.Role](r.db).Find(ctx)
 }
--- back/internal/repositories/user_repository.go
+++ back/internal/repositories/user_repository.go
@@ -15,15 +15,15 @@ func NewUserRepository(db *gorm.DB) *UserRepository {
 	return &UserRepository{db: db}
 }
 
-func (r *UserRepository) Create(ctx context.Context, obj *models.User) error {
+func (r UserRepository) Create(ctx context.Context, obj *models.User) error {
 	return gorm.G[models.User](r.db).Create(ctx, obj)
 }
 
-func (r *UserRepository) Query(ctx context.Context, filter *models.User) ([]models.User, error) {
+func (r UserRepository) Query(ctx context.Context, filter *models.User) ([]models.User, error) {
 	return gorm.G[models.User](r.db).Where(filter).Find(ctx)
 }
 
-func (r *UserRepository) GetByID(ctx context.Context, id uint) (*models.User, error) {
+func (r UserRepository) GetByID(ctx context.Context, id uint) (*models.User, error) {
 	users, err := r.Query(ctx, &models.User{Model: gorm.Model{ID: id}})
 
 	if err != nil {
@@ -37,10 +37,14 @@ func (r *UserRepository) GetByID(ctx context.Context, id uint) (*models.User, er
 	return &users[0], nil
 }
 
-func (r *UserRepository) Update(ctx context.Context, filter *models.User, obj models.User) (int, error) {
+func (r UserRepository) Update(ctx context.Context, filter *models.User, obj models.User) (int, error) {
 	return gorm.G[models.User](r.db).Where(filter).Updates(ctx, obj)
 }
 
-func (r *UserRepository) Delete(ctx context.Context, filter *models.User) (int, error) {
+func (r UserRepository) Delete(ctx context.Context, filter *models.User) (int, error) {
 	return gorm.G[models.User](r.db).Where(filter).Delete(ctx)
+}
+
+func (r UserRepository) GetAll(ctx context.Context) ([]models.User, error) {
+	return gorm.G[models.User](r.db).Find(ctx)
 }
